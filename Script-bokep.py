94aca5ed7261e32cfb5eca4bf27f1a4f44334e7c
xxoprt <86563093+xxoprt@users.noreply.github.com>
2021-08-12 18:04:59 +0700

    Add files via upload

---
+++ Script-bokep.py
@@ -0,0 +1,24 @@
+import os, sys, time
+
+
+os.system('clear')
+
+time.sleep(0.0)
+
+print ('=====================================================')
+print ('[1]. Nonyon.py = Link bokep                             ')
+print ('[2]. Script-Dewasa.py = link Download                   ')
+print ('=====================================================')
+pilih = input('>>>>>>>>>>')
+
+if pilih == 1:
+   os.system('clear')
+   os.system('xdg-open https://www.mediafire.com/download/0gfg7ezqgbcr8gb')
+   print ('NNTKN BOKEP LINK')
+   print ('              ')
+   
+elif pilih == 2:
+     os.system('clear')
+     os.system('xdg-open https://www.mediafire.com/download/nzr89cn1nkgtypr')
+     print ('NNTON BOKEP DOWNLOAD')
+     print ('              ')
